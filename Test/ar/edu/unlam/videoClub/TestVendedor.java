fe94f978ee7ac55c4c110604e1446cd6f6fbe921
AlanForno <aforno@alumno.unlam.edu.ar>
2020-11-10 00:04:43 -0300

    agregue unos Test para vendedor y por ahora todo bien todo correcto

--- Test/ar/edu/unlam/videoClub/TestVendedor.java
+++ Test/ar/edu/unlam/videoClub/TestVendedor.java
@@ -17,14 +17,81 @@ public class TestVendedor {
 	
 	
 	@Test
-	public void queUnVendedorAlquileAUnCliente() {
-		Vendedor nuevoVendedor = new Vendedor("Ramon", 456);
-		Pelicula nuevaPeli = new Pelicula ("SAW 3", 243, 20.0, 16);
-//		Repositor nuevoRepositor = new Repositor ("Jorge", 654);
-		Cliente nuevoCliente = new ClienteMedio ("Marcos", 123, 30, 100.0);
+	public void queUnVendedorPuedaAlquilarAUnCliente() {
+
+		// Se crean los objetos necesarios para alquilar una pelicula
+
+		VideoClub blaze = new VideoClub("Blaze");
+		Empleado nuevoVendedor = new Vendedor("Ramon", 456);
+		Empleado nuevoRepositor = new Repositor("Jorge", 654);
+		Pelicula nuevaPeli = new Pelicula("SAW 3", 243, 20.0, 16);
+		Cliente nuevoCliente = new ClienteBasico("Marcos", 123, 30, 100.0);
+
+		// VideoClub agrega a su lista de empleados Repositor y Vendedor
+
+		blaze.agregarEmpleadoRepositorOVendedor(nuevoRepositor);
+		blaze.agregarEmpleadoRepositorOVendedor(nuevoVendedor);
+
+		// VideoClub agrega a su nuevo cliente
+		blaze.agregarCliente(nuevoCliente);
+
+		// VideoClub agrega la pelicula al stock del negocio
+		blaze.agregarPelicula(nuevoRepositor.getCodigoEmpleado(), nuevaPeli);
+
+		Boolean sePudoAlquilarLaPelicula;
+		Integer peliculasEnAlquiler = 1;
 		
-		nuevoCliente.alquilarPelicula(nuevaPeli);
-		assertTrue(nuevoVendedor.alquilarPelicula(nuevaPeli, nuevoCliente));
+		// Se realiza el casteo a tipo Vendedor y su utiliza el metodo para alquilarle
+		// al cliente una pelicula
+		sePudoAlquilarLaPelicula = ((Vendedor) nuevoVendedor).alquilarACliente(blaze.getListaCliente(), nuevoCliente,
+				blaze.listaDePeliculas(), nuevaPeli, blaze.getListaDePeliculasAlquiladas());
+
+		assertTrue(sePudoAlquilarLaPelicula);
+		
+		// Aca se evalua si la cantidad de peliculas alquiladas es 1
+		assertEquals(peliculasEnAlquiler, ((Vendedor) nuevoVendedor).getCantidadPeliculasAlquiladas());
+
+	}
+	
+	@Test
+	public void queUnVendedorRecibaUnaPeliculaDelCliente() {
+		// Se crean los objetos necesarios para alquilar una pelicula
+
+		VideoClub blaze = new VideoClub("Blaze");
+		Empleado nuevoVendedor = new Vendedor("Ramon", 456);
+		Empleado nuevoRepositor = new Repositor("Jorge", 654);
+		Pelicula nuevaPeli = new Pelicula("SAW 3", 243, 20.0, 16);
+		Cliente nuevoCliente = new ClienteBasico("Marcos", 123, 30, 100.0);
+		
+		Boolean sePudoAlquilarLaPelicula;
+		Boolean sePudoDevolverLaPelicula;
+		Integer peliculasEnAlquiler = 0;
+
+		// VideoClub agrega a su lista de empleados Repositor y Vendedor
+
+		blaze.agregarEmpleadoRepositorOVendedor(nuevoRepositor);
+		blaze.agregarEmpleadoRepositorOVendedor(nuevoVendedor);
+
+		// VideoClub agrega a su nuevo cliente
+		blaze.agregarCliente(nuevoCliente);
+
+		// VideoClub agrega la pelicula al stock del negocio
+		blaze.agregarPelicula(nuevoRepositor.getCodigoEmpleado(), nuevaPeli);
+		
+		// El vendedor le alquila la pelicula al cliente (ahora la pelicula esta en la listaDePeliculas de Cliente)
+		sePudoAlquilarLaPelicula = ((Vendedor) nuevoVendedor).alquilarACliente(blaze.getListaCliente(), nuevoCliente,
+				blaze.listaDePeliculas(), nuevaPeli, blaze.getListaDePeliculasAlquiladas());
+		
+		assertTrue(sePudoAlquilarLaPelicula);
+		
+		// El cliente devuelve la pelicula que le alquilo al vendedor anteriormente
+		sePudoDevolverLaPelicula = ((Vendedor) nuevoVendedor).recibirPelicula(blaze.getListaCliente(), nuevoCliente,
+				blaze.listaDePeliculas(), nuevaPeli, blaze.getListaDePeliculasAlquiladas());
+		
+		assertTrue(sePudoDevolverLaPelicula);
+		
+		// Aca se evalua si la cantidad de peliculas alquiladas es 0 (porque se devolvio la pelicula)
+		assertEquals(peliculasEnAlquiler, ((Vendedor) nuevoVendedor).getCantidadPeliculasAlquiladas());
 	}
 
 }
